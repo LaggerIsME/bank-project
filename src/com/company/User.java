fa51ca3613b0d77a3a138767e1bebfbf6d864987
LaggerIsME <abajsadykov2003@gmail.com>
2022-02-16 16:45:23 +0600

    aeb

---
+++ src/com/company/User.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class User {
+    private String surname;
+    private String name;
+    private String number;
+    private int id;
+    private String password;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ src/com/company/Wallet.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Wallet {
+    private Double money;
+    private Double deposit;
+    private Double credit;
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public Double getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(Double deposit) {
+        this.deposit = deposit;
+    }
+
+    public Double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(Double credit) {
+        this.credit = credit;
+    }
+}
